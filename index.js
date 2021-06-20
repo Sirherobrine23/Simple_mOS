8dd478dbfe6aedbe526b2782edd3c34ebbd746e3
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2021-06-20 20:28:30 +0000

    Push Modifications

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { resolve, join } = require("path");
 const { existsSync, readFileSync, writeFileSync, rmSync, appendFileSync } = require("fs");
 const { cpus, freemem } = require("os")
-const { cwd, exit } = require("process");
+const { cwd, exit, geteuid } = require("process");
 const { exec, execSync } = require("child_process");
 
 const yaml = {parse: require("js-yaml").load, stringify: require("js-yaml").dump};
@@ -60,67 +60,72 @@ else {
     process.exit(0)
 }
 
+console.log("\n------------- System Spec");
 const CpuFamily = ConfigsQemu.cpu[OSConfig.spec.cpu.family]
 // Ram Family Limit
-if (CpuFamily.ram >= OSConfig.spec.ram) {console.log("RAM limit reached for this family"); OSConfig.spec.ram = CpuFamily.ram}
+if (CpuFamily.ram < OSConfig.spec.ram.MEM_MB) {
+    console.log("RAM limit reached for this family");
+    OSConfig.spec.ram.MEM_MB = CpuFamily.ram
+}
 // Core Family Limit
-if (CpuFamily.core >= OSConfig.spec.cores) {console.log("Number of Cores reached for this processor family"); OSConfig.spec.cores = CpuFamily.core}
+if (CpuFamily.core > OSConfig.spec.cpu.cores) {
+    console.log("Number of Cores reached for this processor family");
+    OSConfig.spec.cpu.cores = CpuFamily.core
+}
 // Threads Family Limit
-if (CpuFamily.threads >= OSConfig.spec.threads) {console.log("Number of Threads Reaching Processor Family Limit"); OSConfig.spec.threads = CpuFamily.threads}
-
+if (CpuFamily.threads < OSConfig.spec.cpu.threads) {
+    console.log("Number of Threads Reaching Processor Family Limit");
+    OSConfig.spec.cpu.threads = CpuFamily.threads
+}
 const Opencore_Path = resolve(__dirname, "MacOS/OpenCORE", OSConfig.spec.cpu.family);
 const BiosBootPath = resolve(__dirname, "MacOS/PFLASH")
-
-console.log("\n\n------------- System Spec");
-
-
+console.log(`
+RAM: ${parseInt(OSConfig.spec.ram.MEM_MB)},
+CPU Cores: ${OSConfig.spec.cpu.cores},
+Threads: ${OSConfig.spec.cpu.threads},
+Sockets: ${OSConfig.spec.cpu.sockets}
+`);
 
 RunQemu.push(
     `-enable-kvm`,
-    `-m ${parseInt(OSConfig.spec.ram.MEM_MB)}`,
-    `-cpu ${CpuFamily.cpu_name},kvm=on,vendor=GenuineIntel,+invtsc,vmware-cpuid-freq=on,${OSConfig.spec.cpu.options}`,
-    `-machine q35`,
-    "-usb -device usb-kbd -device usb-tablet",
+    `-m ${parseInt(OSConfig.spec.ram.MEM_MB)} -cpu ${CpuFamily.cpu_name},kvm=on,vendor=GenuineIntel,+invtsc,vmware-cpuid-freq=on,${OSConfig.spec.cpu.options}`,
     `-smp ${OSConfig.spec.cpu.threads},cores=${OSConfig.spec.cpu.cores},sockets=${OSConfig.spec.cpu.sockets}`,
-    "-device usb-ehci,id=ehci",
+    "-machine q35 -usb -device usb-kbd -device usb-tablet -device usb-ehci,id=ehci",
     `-device isa-applesmc,osk="ourhardworkbythesewordsguardedpleasedontsteal(c)AppleComputerInc"`,
-    `-drive if=pflash,format=raw,readonly=on,file="${join(BiosBootPath, "OVMF_CODE.fd")}"`,
-    `-drive if=pflash,format=raw,file="${join(BiosBootPath, "OVMF_VARS-1024x768.fd")}"`,
-    "-smbios type=2",
-    "-device ich9-intel-hda -device hda-duplex",
-    "-device ich9-ahci,id=sata",
-    `-drive id=OpenCoreBoot,if=none,snapshot=on,format=qcow2,file="${join(Opencore_Path, "OpenCore.qcow2")}"`,
-    "-device ide-hd,bus=sata.2,drive=OpenCoreBoot",
+    `-drive if=pflash,format=raw,readonly=on,file="${join(BiosBootPath, "OVMF_CODE.fd")}" -drive if=pflash,format=raw,file="${join(BiosBootPath, "OVMF_VARS-1024x768.fd")}"`,
+    "-smbios type=2 -device ich9-intel-hda -device hda-duplex -device ich9-ahci,id=sata",
+    `-drive id=OpenCoreBoot,if=none,snapshot=on,format=qcow2,file="${join(Opencore_Path, "OpenCore.qcow2")}" -device ide-hd,bus=sata.0,drive=OpenCoreBoot`,
     `-netdev user,id=net0 -device vmxnet3,netdev=net0,id=net0,mac=${OSConfig.network.mac}`,
     "-monitor stdio"
 )
 
-console.log("\n\n------------- Storage");
+console.log("\n------------- Storage");
 // Storage
 for (let _HDD of OSConfig.storage){
     // const HDD = ConfigsQemu.harddisk[_HDD.TYPE]
     var HDD = ConfigsQemu.harddisk[_HDD.TYPE]
     // Create Image
-    if (!(existsSync(_HDD.PATH)) && (HDD.type === "virtual")) execSync(`qemu-img create -f ${_HDD.TYPE} ${_HDD.PATH} ${_HDD.SIZE_GB}G`);
-    var command = HDD.command.split("${{PATH}}").join(_HDD.PATH)
-        .split("${{ID}}").join(_HDD.QEMU_ID)
-        .split("${{SATA_ID}}").join(_HDD.SATA_ID)
+    var command = ""
+    if (!(existsSync(_HDD.PATH)) && (HDD.type === "virtual")) execSync(`qemu-img create -f ${_HDD.TYPE || "qcow2"} ${_HDD.PATH} ${_HDD.SIZE_GB || 128}G`);
+    if (existsSync(_HDD.PATH) && (HDD.type === "hardware")) {if (geteuid() !== 0) console.log("Run with root or sudo -E");}
+    command = HDD.command.split("${{PATH}}").join(_HDD.PATH).split("${{ID}}").join(_HDD.QEMU_ID || HDD.options.ID.default).split("${{SATA_ID}}").join(_HDD.SATA_ID || HDD.options.SATA_ID.default)
     console.log(command);
     RunQemu.push(command)
 }
 
-console.log("\n\n------------- Video");
+console.log("\n------------- Video");
 // Video
-if (OSConfig.video.type.toLocaleLowerCase() === "vga") RunQemu.push(`-device VGA,vgamem_mb=${OSConfig.video.mem || 128}`);
-else {
+if (OSConfig.video.type.toLocaleLowerCase() === "vga"){
+    console.log(`Starting MacOS with ${OSConfig.video.MEM_MB || ConfigsQemu.video.vga.options.MEM.default}MB of vram with vga`)
+    RunQemu.push(ConfigsQemu.video.vga.command.split("${{MEM}}").join(OSConfig.video.MEM_MB || ConfigsQemu.video.vga.options.MEM.default));
+} else {
     console.log("Starting MacOS with vnc on all network interfaces")
     RunQemu.push("-vnc 0.0.0.0:0")
 }
 
 if (OSConfig.Install.enable === true) {
-    console.log("\n\n------------- Download MacOS Installer");
+    console.log("\n------------- Download MacOS Installer");
     if (existsSync(OSConfig.Install.system.path)) rmSync(OSConfig.Install.system.path, {force: true})
-    // console.log("---------------------------------\n\n");
     const __MacOSDOwnload = FetchOS(OSConfig.Install.system.name)
     console.log("Converting from dmg to img");
     const __MacDMGConvert = execSync(`qemu-img convert ${__MacOSDOwnload.System} -O raw ${OSConfig.Install.system.path}`).toString()
@@ -139,9 +144,6 @@ if (OSConfig.Install.enable === true) {
 // Run Qemu
 console.log("starting qemu");
 const CommandExec = `qemu-system-x86_64 ${RunQemu.join(" ")}`
-
-console.log(CommandExec);
-
 const RunQemuCommand = exec(CommandExec, {cwd: CurrentPath});
 const LogPath = join(CurrentPath, "KVMQemu.log")
 if (OSConfig.spec.SaveLog) writeFileSync(LogPath, `Command Exec: ${CommandExec}\n\n`)
